c96bb9bf8189b660d3880dade706051130fde73a
Barry Linkletter <blinkletter@upei.ca>
2024-07-15 22:05:06 -0300

    hghg

--- MyFolder/program.py
+++ MyFolder/program.py
@@ -1,15 +1,15 @@
 import scipy.constants as spc
-import numpy as np              # math tools
+import numpy as np 
 
-c = spc.c * 100          # speed of light: convert m.s^-1 to cm.s^-1
-pi = spc.pi              # pi to many decimal places
-N = spc.Avogadro         # Avogadro's number (mole^-1)
+c = spc.c * 100  
+pi = spc.pi           
+N = spc.Avogadro   
 
-k = 5E5      # 5x10^5 dynes/cm -- the force constant of a C-H bond stretch
-m1 = 1 / N   # mass of an individual carbon atom
-m2 = 12 / N  # mass of an individual hydrogen atom
+k = 5E5     
+m1 = 1 / N   
+m2 = 12 / N 
 
-u = m1*m2 / (m1+m2)      # reduced mass
-v = 1 / (2*pi*c) * np.sqrt(k/u)   # Hooke's law
+u = m1*m2 / (m1+m2)     
+v = 1 / (2*pi*c) * np.sqrt(k/u) 
 
 print(f"The frequency is {v:0.1f} cm^-1")
